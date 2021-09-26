49c8a53db07e1538e0e437672b97bb9a23f27869
samrelins <samrelins@gmail.com>
2021-09-26 15:06:24 +0100

    updated comments in .md solutions

--- solution_1.sh
+++ solution_1.sh
@@ -1,11 +1,18 @@
-# There are multiple ways one can rename files, though doing so using a graphical user interface type environment can be laborious
+# There are multiple ways one can rename files, though doing so using a 
+# graphical user interface type environment can be laborious
 
-# the following is a short script that you can run inside a linux terminal environment
-# the first and last lines instantiate a simple for loop that iterate over every file in the current working directory
-# the middle line uses the bash "mv" command to rename the files: "$file" is the current file being iterated over, $(basename "$file" .csv) returns the filename without any suffixes or the .csv extension, the .dat at the end adds the required extension
+# The following is a short script that you can run inside a linux terminal 
+# environment. The first and last lines instantiate a simple for loop that 
+# iterate over every file in the current working directory. The middle line 
+# uses the bash "mv" command to rename the files: "$file" is the current file 
+# being iterated over, $(basename "$file" .csv) returns the filename without 
+# any suffixes or the .csv extension, the .dat at the end adds the required 
+# extension.
 
 for file in *.csv; do
 	mv "$file" "$(basename "$file" .csv).dat"
 done
 
-# the above can be typed manually in the directory containing the files with the incorrect extension, or this .sh file can be copied to the directory and run using the command "$ sh FILENAME.sh"
+# the above can be typed manually in the directory containing the files with 
+# the incorrect extension, or this .sh file can be copied to the directory and 
+# run using the command "$ sh FILENAME.sh"
--- solution_2.sh
+++ solution_2.sh
@@ -1,16 +1,23 @@
-# one can quickly see the number of words in a raw text file using the "wc" linux/bash command. 
-# type the following into as a terminal command, in the directory containing the files in question:
+# One can quickly see the number of words in a raw text file using the "wc" 
+# linux/bash command. Type the following into as a terminal command, in the 
+# directory containing the files in question:
 
 wc -w *
 
-# the -w option specifies that words should be counted, the * is a wildcard character indicating that the command should be applied to all files in the current working directory
+# The -w option specifies that words should be counted, the * is a wildcard 
+# character indicating that the command should be applied to all files in the 
+# current working directory
 
-# the output is a list of all the files in the working directory and their word counts, plus a total 
-# which in the case of the current example, this is pretty verbose
+# The output is a list of all the files in the working directory and their 
+# word counts, plus a total which in the case of the current example, this is 
+# pretty verbose
 
-# to avoid having to scroll down a long list of files, you can "pipe" or pass the output directly into a grep command 
-# grep can be used to show all the lines of output that don't contain "100", or "total" in the case of the final line of output from wc, like so: 
+# To avoid having to scroll down a long list of files, you can "pipe" or pass 
+# the output directly into a grep command grep can be used to show all the 
+# lines of output that don't contain "100", or "total" in the case of the 
+# final line of output from wc, like so: 
  
 wc -w * | grep "100\|total" -v
 
-# The above will show any files in the current working directory that don't contain 100 words
+# The above will show any files in the current working directory that don't 
+# contain 100 words
